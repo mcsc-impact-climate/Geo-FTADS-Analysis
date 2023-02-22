627fbdd1e5b128a02b0f9e064c724beb8cf8e85c
Danika MacDonell <danikamacdonell@dhcp-10-29-40-169.dyn.MIT.EDU>
2023-02-22 18:50:50 -0500

    Updates to remove absolute paths from AnalyzeFAFData.py

--- source/AnalyzeFAFData.py
+++ source/AnalyzeFAFData.py
@@ -1,119 +1,142 @@
-
-
+# Import needed modules
 from qgis.core import *
 import qgis.utils
 from qgis.PyQt import QtGui
+import os
+from console.console import _console
+import sys
 
-######################################## Load and process FAF5 regions #########################################
-# Load the FAF5 regions shapefile and add it to the registry if it isn't already there
-regions = QgsVectorLayer('/Users/danikamacdonell/GIS/QGIS/FAF5/Freight_Analysis_Framework_(FAF5)_Regions/Freight_Analysis_Framework_(FAF5)_Regions.shp', 'FAF5 Regions', 'ogr')
-
-# Confirm that the regions got loaded in correctly
-if not regions.isValid():
-  print("Layer failed to load!")
-  
-# Add the regions to the map if they aren't already there
-if not QgsProject.instance().mapLayersByName("FAF5 Regions"):
-    QgsProject.instance().addMapLayer(regions)
+def main():
     
-# Apply a filter to only show regions in Texas
-regions.setSubsetString('FAF_Zone_D LIKE \'%TX%\'')
+    # Get the path to top level of the git directory so we can use relative paths
+    source_dir = os.path.dirname(_console.console.tabEditorWidget.currentWidget().path)
+    print(source_dir)
+    if source_dir.endswith('/source'):
+        top_dir = source_dir[:-7]
+    elif source_dir.endswith('/source/'):
+        top_dir = source_dir[:-8]
+    else:
+        print("ERROR: Expect current directory to end with 'source'. Cannot use relative directories as-is. Exiting...")
+        sys.exitfunc()
+        
+        
+    print(top_dir)
 
-# Give each FAF zone a different color
-field_name = 'FAF_Zone_D'
-field_index = regions.fields().indexFromName(field_name)
-unique_values = regions.uniqueValues(field_index)
+    """
+    ######################################## Load and process FAF5 regions #########################################
+    # Load the FAF5 regions shapefile and add it to the registry if it isn't already there
+    print(f'{top_dir}/FAF5_zones/Freight_Analysis_Framework_(FAF5)_Regions.shp')
+    regions = QgsVectorLayer(f'{top_dir}/FAF5_zones/Freight_Analysis_Framework_(FAF5)_Regions.shp', 'FAF5 Regions', 'ogr')
 
-category_list = []
-for value in unique_values:
-    symbol = QgsSymbol.defaultSymbol(regions.geometryType())
-    category = QgsRendererCategory(value, symbol, str(value))
-    category_list.append(category)
+    # Confirm that the regions got loaded in correctly
+    if not regions.isValid():
+      print('Layer failed to load!')
+      
+    # Add the regions to the map if they aren't already there
+    if not QgsProject.instance().mapLayersByName('FAF5 Regions'):
+        QgsProject.instance().addMapLayer(regions)
+        
+    # Apply a filter to only show regions in Texas
+    regions.setSubsetString('FAF_Zone_D LIKE \'%TX%\'')
 
-renderer = QgsCategorizedSymbolRenderer(field_name, category_list)
-regions.setRenderer(renderer)
-regions.triggerRepaint()
-################################################################################################################
+    # Give each FAF zone a different color
+    field_name = 'FAF_Zone_D'
+    field_index = regions.fields().indexFromName(field_name)
+    unique_values = regions.uniqueValues(field_index)
 
-##################################### Load and process FAF5 netowrk links ######################################
-# Load in the FAF5 network links shapefile
-links = QgsVectorLayer('/Users/danikamacdonell/GIS/QGIS/FAF5/Freight_Analysis_Framework_(FAF5)_Network_Links/Freight_Analysis_Framework_(FAF5)_Network_Links.shp', 'FAF5 Links', 'ogr')
+    category_list = []
+    for value in unique_values:
+        symbol = QgsSymbol.defaultSymbol(regions.geometryType())
+        category = QgsRendererCategory(value, symbol, str(value))
+        category_list.append(category)
 
-# Confirm that the netowrk links got loaded in correctly
-if not links.isValid():
-  print("Layer failed to load!")
+    renderer = QgsCategorizedSymbolRenderer(field_name, category_list)
+    regions.setRenderer(renderer)
+    regions.triggerRepaint()
+    ################################################################################################################
 
-# Add the links to the map if they aren't already there
-if not QgsProject.instance().mapLayersByName("FAF5 Links"):
-    QgsProject.instance().addMapLayer(links)
-    
-# Apply a filter to only show links in Texas
-links.setSubsetString('STATE LIKE \'%TX%\'')
-################################################################################################################
 
-################################## Load and process FAF5 highway assignments ###################################
-# Read in the total highway trucking flows by commodity for 2022
-uri = '/Users/danikamacdonell/GIS/QGIS/FAF5/FAF5_Highway_Assignment_Results/FAF5_2022_Highway_Assignment_Results/CSV Format/FAF5 Total Truck Flows by Commodity_2022.csv'
-assignments = QgsVectorLayer(uri, 'FAF5 Assignments', 'ogr')
+    ##################################### Load and process FAF5 netowrk links ######################################
+    # Load in the FAF5 network links shapefile
+    links = QgsVectorLayer('/Users/danikamacdonell/GIS/QGIS/FAF5/Freight_Analysis_Framework_(FAF5)_Network_Links/Freight_Analysis_Framework_(FAF5)_Network_Links.shp', 'FAF5 Links', 'ogr')
 
-# Skim the assignments down to what we want to work with, and make sure the variable types are correct (by default QGIS reads in CSV data as strings, even if they're actually numbers)
-# Note:
-proc = processing.run("native:refactorfields", {'INPUT':'/Users/danikamacdonell/GIS/QGIS/FAF5/FAF5_Highway_Assignment_Results/FAF5_2022_Highway_Assignment_Results/CSV Format/FAF5 Total Truck Flows by Commodity_2022.csv','FIELDS_MAPPING':[{'expression': '"ID"','length': 0,'name': 'ID','precision': 0,'sub_type': 0,'type': 2,'type_name': 'integer'},{'expression': '"TOT Tons_22 All"','length': 0,'name': 'TOT Tons_22 All','precision': 0,'sub_type': 0,'type': 6,'type_name': 'double precision'}],'OUTPUT':'TEMPORARY_OUTPUT'})
-assignments = proc['OUTPUT']
+    # Confirm that the netowrk links got loaded in correctly
+    if not links.isValid():
+      print('Layer failed to load!')
 
-# Confirm that the highway assignments got loaded in correctly
-if not assignments.isValid():
-  print("Layer failed to load!")
+    # Add the links to the map if they aren't already there
+    if not QgsProject.instance().mapLayersByName('FAF5 Links'):
+        QgsProject.instance().addMapLayer(links)
+        
+    # Apply a filter to only show links in Texas
+    links.setSubsetString('STATE LIKE \'%TX%\'')
+    ################################################################################################################
 
-# Add the highway assignments to the QGIS project
-if not QgsProject.instance().mapLayersByName("Refactored"):
-    QgsProject.instance().addMapLayer(assignments)
-################################################################################################################
-    
-################## Join the FAF5 assignments to the highway networks via the highway link IDs ##################
-#assignments = iface.activeLayer()
+    ################################## Load and process FAF5 highway assignments ###################################
+    # Read in the total highway trucking flows by commodity for 2022
+    uri = '/Users/danikamacdonell/GIS/QGIS/FAF5/FAF5_Highway_Assignment_Results/FAF5_2022_Highway_Assignment_Results/CSV Format/FAF5 Total Truck Flows by Commodity_2022.csv'
+    assignments = QgsVectorLayer(uri, 'FAF5 Assignments', 'ogr')
 
-# Initialize a vector layer join info object to specify how the join will be performed
-info = QgsVectorLayerJoinInfo()
+    # Skim the assignments down to what we want to work with, and make sure the variable types are correct (by default QGIS reads in CSV data as strings, even if they're actually numbers)
+    # Note:
+    proc = processing.run("native:refactorfields", {'INPUT':'/Users/danikamacdonell/GIS/QGIS/FAF5/FAF5_Highway_Assignment_Results/FAF5_2022_Highway_Assignment_Results/CSV Format/FAF5 Total Truck Flows by Commodity_2022.csv','FIELDS_MAPPING':[{'expression': '"ID"','length': 0,'name': 'ID','precision': 0,'sub_type': 0,'type': 2,'type_name': 'integer'},{'expression': '"TOT Tons_22 All"','length': 0,'name': 'TOT Tons_22 All','precision': 0,'sub_type': 0,'type': 6,'type_name': 'double precision'}],'OUTPUT':'TEMPORARY_OUTPUT'})
+    assignments = proc['OUTPUT']
 
-# The common field to be used for joining is the network link ID (called 'ID' in both the assignments and links layers)
-info.setJoinFieldName("ID")
-info.setTargetFieldName("ID")
-info.setJoinLayer(assignments)
+    # Confirm that the highway assignments got loaded in correctly
+    if not assignments.isValid():
+      print('Layer failed to load!')
 
-# Specify that the joined layer will be stored in memory for ready access
-info.setUsingMemoryCache(True)
+    # Add the highway assignments to the QGIS project
+    if not QgsProject.instance().mapLayersByName('Refactored'):
+        QgsProject.instance().addMapLayer(assignments)
+    ################################################################################################################
+        
+    ################## Join the FAF5 assignments to the highway networks via the highway link IDs ##################
+    #assignments = iface.activeLayer()
 
-# Apply the join to the network links layer
-links.addJoin(info)
-################################################################################################################
+    # Initialize a vector layer join info object to specify how the join will be performed
+    info = QgsVectorLayerJoinInfo()
 
-######################### Style the network links to have width proportional to tonnage ########################
-# Target field is by default the total tons transported as weighting factor for network link widths
-myTargetField = 'Refactored_TOT Tons_22 All'
+    # The common field to be used for joining is the network link ID (called 'ID' in both the assignments and links layers)
+    info.setJoinFieldName('ID')
+    info.setTargetFieldName('ID')
+    info.setJoinLayer(assignments)
 
-# Specify the range of possible weighting factors as the max and min range of the target field
-ramp_max = links.maximumValue(links.fields().indexFromName(myTargetField))
-ramp_min = links.minimumValue(links.fields().indexFromName(myTargetField))
+    # Specify that the joined layer will be stored in memory for ready access
+    info.setUsingMemoryCache(True)
 
-# Classify the tonnages into 5 bins
-ramp_num_steps = 5
+    # Apply the join to the network links layer
+    links.addJoin(info)
+    ################################################################################################################
 
-# Initialize a graduated renderer object specify how the network link symbol properties will vary according to the numerical target field
-renderer = QgsGraduatedSymbolRenderer(myTargetField)
-renderer.setClassAttribute(myTargetField)
+    ######################### Style the network links to have width proportional to tonnage ########################
+    # Target field is by default the total tons transported as weighting factor for network link widths
+    myTargetField = 'Refactored_TOT Tons_22 All'
 
-# Specify the network link symbol to be a black line in all cases
-symbol = QgsSymbol.defaultSymbol(links.geometryType())
-symbol.setColor(QtGui.QColor('#000000'))
-renderer.setSourceSymbol(symbol)
+    # Specify the range of possible weighting factors as the max and min range of the target field
+    ramp_max = links.maximumValue(links.fields().indexFromName(myTargetField))
+    ramp_min = links.minimumValue(links.fields().indexFromName(myTargetField))
 
-# Specify the binning method (jenks), number of bins, and range of line widths for the graduated renderer
-renderer.setClassificationMethod(QgsClassificationJenks())
-renderer.updateClasses(links, ramp_num_steps)
-renderer.setSymbolSizes(0.1, 3)
-#renderer.updateColorRamp()
+    # Classify the tonnages into 5 bins
+    ramp_num_steps = 5
 
-# Apply the graduated symbol renderer defined above to the network links
-links.setRenderer(renderer)
-################################################################################################################
+    # Initialize a graduated renderer object specify how the network link symbol properties will vary according to the numerical target field
+    renderer = QgsGraduatedSymbolRenderer(myTargetField)
+    renderer.setClassAttribute(myTargetField)
+
+    # Specify the network link symbol to be a black line in all cases
+    symbol = QgsSymbol.defaultSymbol(links.geometryType())
+    symbol.setColor(QtGui.QColor('#000000'))
+    renderer.setSourceSymbol(symbol)
+
+    # Specify the binning method (jenks), number of bins, and range of line widths for the graduated renderer
+    renderer.setClassificationMethod(QgsClassificationJenks())
+    renderer.updateClasses(links, ramp_num_steps)
+    renderer.setSymbolSizes(0.1, 3)
+    #renderer.updateColorRamp()
+
+    # Apply the graduated symbol renderer defined above to the network links
+    links.setRenderer(renderer)
+    ################################################################################################################
+    """
+main()
