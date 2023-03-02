bafc4d24c67778856d07ff11acbbcad61a3825b7
cubicalknight <micah.borrero@gmail.com>
2023-03-02 14:56:06 -0500

    added way to streamline loading in multiple states

--- source/AnalyzeFAFData.py
+++ source/AnalyzeFAFData.py
@@ -6,19 +6,22 @@ import os
 from console.console import _console
 import sys
 
-def main():
-    
-    # Get the path to top level of the git directory so we can use relative paths
-    source_dir = os.path.dirname(_console.console.tabEditorWidget.currentWidget().path)
-    if source_dir.endswith('/source'):
-        top_dir = source_dir[:-7]
-    elif source_dir.endswith('/source/'):
-        top_dir = source_dir[:-8]
-    else:
-        print("ERROR: Expect current directory to end with 'source'. Cannot use relative directories as-is. Exiting...")
-        sys.exitfunc()
 
-    ######################################## Load and process FAF5 regions #########################################
+# Get the path to top level of the git directory so we can use relative paths
+source_dir = os.path.dirname(_console.console.tabEditorWidget.currentWidget().path)
+if source_dir.endswith('/source'):
+    top_dir = source_dir[:-7]
+elif source_dir.endswith('/source/'):
+    top_dir = source_dir[:-8]
+else:
+    print("ERROR: Expect current directory to end with 'source'. Cannot use relative directories as-is. Exiting...")
+    sys.exitfunc()
+
+
+def loadRegions(st):
+    '''
+    Load and process FAF5 regions
+    '''
     # Load the FAF5 regions shapefile and add it to the registry if it isn't already there
     regions = QgsVectorLayer(f'{top_dir}/data/FAF5_regions/Freight_Analysis_Framework_(FAF5)_Regions.shp', 'FAF5 Regions', 'ogr')
 
@@ -29,9 +32,11 @@ def main():
     # Add the regions to the map if they aren't already there
     if not QgsProject.instance().mapLayersByName('FAF5 Regions'):
         QgsProject.instance().addMapLayer(regions)
-        
-    # Apply a filter to only show regions in Texas
-    regions.setSubsetString('FAF_Zone_D LIKE \'%TX%\'')
+
+    passin = f'FAF_Zone_D LIKE {st}'
+    # print(passin, len(states))
+    # Apply a filter to only show regions in 'State'
+    regions.setSubsetString(passin)
 
     # Give each FAF zone a different color
     field_name = 'FAF_Zone_D'
@@ -47,10 +52,12 @@ def main():
     renderer = QgsCategorizedSymbolRenderer(field_name, category_list)
     regions.setRenderer(renderer)
     regions.triggerRepaint()
-    ################################################################################################################
 
 
-    ##################################### Load and process FAF5 netowrk links ######################################
+def loadNetworkLinks(st):
+    ''' 
+    Load and process FAF5 network links 
+    '''
     # Load in the FAF5 network links shapefile
     links = QgsVectorLayer(f'{top_dir}/data/FAF5_network_links/Freight_Analysis_Framework_(FAF5)_Network_Links.shp', 'FAF5 Links', 'ogr')
 
@@ -62,30 +69,44 @@ def main():
     if not QgsProject.instance().mapLayersByName('FAF5 Links'):
         QgsProject.instance().addMapLayer(links)
         
-    # Apply a filter to only show links in Texas
-    links.setSubsetString('STATE LIKE \'%TX%\'')
-    ################################################################################################################
-
-
-
-    ################################## Load and process FAF5 highway assignments ###################################
+    # Apply a filter to only show links in 'State'
+    links.setSubsetString(f'STATE LIKE {st}')
+    
+    
+def loadHighways():
+    '''
+    Load and process FAF5 highway assignments
+    '''
     # Read in the total highway trucking flows by commodity for 2022
-    uri = f'{top_dir}/data/FAF5_Highway_Assignment_Results/FAF5_2022_Highway_Assignment_Results/CSV Format/FAF5 Total Truck Flows by Commodity_2022.csv'
+    uri = f'{top_dir}/data/FAF5_highway_assignment_results/FAF5_2022_Highway_Assignment_Results/CSV Format/FAF5 Total Truck Flows by Commodity_2022.csv'
     assignments = QgsVectorLayer(uri, 'FAF5 Assignments', 'ogr')
-
+    
     # Skim the assignments down to what we want to work with, and make sure the variable types are correct (by default QGIS reads in CSV data as strings, even if they're actually numbers)
     # Note:
     proc = processing.run("native:refactorfields", {'INPUT':uri,'FIELDS_MAPPING':[{'expression': '"ID"','length': 0,'name': 'ID','precision': 0,'sub_type': 0,'type': 2,'type_name': 'integer'},{'expression': '"TOT Tons_22 All"','length': 0,'name': 'TOT Tons_22 All','precision': 0,'sub_type': 0,'type': 6,'type_name': 'double precision'}],'OUTPUT':'TEMPORARY_OUTPUT'})
     assignments = proc['OUTPUT']
-
+    
     # Confirm that the highway assignments got loaded in correctly
     if not assignments.isValid():
       print('Layer failed to load!')
-
+    
     # Add the highway assignments to the QGIS project
     if not QgsProject.instance().mapLayersByName('Refactored'):
         QgsProject.instance().addMapLayer(assignments)
-    ################################################################################################################
+    
+
+def main():
+    states = ['CA', 'NV']
+    st = ''
+    for i in range(len(states)):
+        st += '\'%' + states[i] + '%\''
+        if i < len(states)-1:
+            st += ' AND '
+    
+    loadRegions(st)
+    loadNetworkLinks(st)
+    loadHighways()
+    
 
     ################## Join the FAF5 assignments to the highway networks via the highway link IDs ##################
     #assignments = iface.activeLayer()
