974c879a948769ecf66985e5c76f736849d1ab73
Danika MacDonell <danikamacdonell@Danikas-MacBook-Pro-2.local>
2023-04-08 23:25:45 -0400

    Updated evaluation of highway network assignments to perform with geopandas rather than QGIS

--- source/PlotWithQGIS.py
+++ source/PlotWithQGIS.py
@@ -56,9 +56,77 @@ def readRegions(path, name):
     # Apply a filter for the moment to remove Hawaii and Alaska
     regions.setSubsetString('NOT FAF_Zone_D LIKE \'%Alaska%\' AND NOT FAF_Zone_D LIKE \'%HI%\'')
 
-    print(type(regions))
-
     return regions
+    
+def load_highway_links(st=None):
+    '''
+    Loads in the highway network links produced by HighwayAssignmentTools.py with the FAF5 flux data included.
+
+    Parameters
+    ----------
+    path_to_links (string): Path to the shapefile containing the highway links
+
+    Returns
+    -------
+    None
+    '''
+
+    # Load in the FAF5 network links shapefile
+    links = QgsVectorLayer(f'{top_dir}/data/highway_assignment_links/highway_assignment_links.shp', 'FAF5 Links', 'ogr')
+    
+    # Confirm that the netowrk links got loaded in correctly
+    if not links.isValid():
+        print('Highway links failed to load!')
+        
+    # Add the links to the map if they aren't already there
+    if not QgsProject.instance().mapLayersByName('Total Tons Transported by Highway'):
+        QgsProject.instance().addMapLayer(links)
+    
+    # Apply a filter to only show links in the given state
+    if st is not None: links.setSubsetString(st)
+        
+    return links
+    
+def style_highway_links(links):
+    '''
+    Styles the highway links to make the width of each link proportional to the total annual flux carried
+
+    Parameters
+    ----------
+    links (class 'qgis._core.QgsVectorLayer'): Highway network links, including attribute quantifying annual flux carried over each link
+
+    Returns
+    -------
+    None
+    '''
+    # Target field is by default the total tons transported as weighting factor for network link widths
+    #myTargetField = 'Refactored_TOT Tons_22 All'
+    myTargetField = 'TOT Tons_2'
+
+    # Specify the range of possible weighting factors as the max and min range of the target field
+    ramp_max = links.maximumValue(links.fields().indexFromName(myTargetField))
+    ramp_min = links.minimumValue(links.fields().indexFromName(myTargetField))
+
+    # Classify the tonnages into 5 bins
+    ramp_num_steps = 5
+
+    # Initialize a graduated renderer object specify how the network link symbol properties will vary according to the numerical target field
+    renderer = QgsGraduatedSymbolRenderer(myTargetField)
+    renderer.setClassAttribute(myTargetField)
+
+    # Specify the network link symbol to be a black line in all cases
+    symbol = QgsSymbol.defaultSymbol(links.geometryType())
+    symbol.setColor(QtGui.QColor('#000000'))
+    renderer.setSourceSymbol(symbol)
+
+    # Specify the binning method (jenks), number of bins, and range of line widths for the graduated renderer
+    renderer.setClassificationMethod(QgsClassificationJenks())
+    renderer.updateClasses(links, ramp_num_steps)
+    renderer.setSymbolSizes(0.1, 2)
+    #renderer.updateColorRamp()
+
+    # Apply the graduated symbol renderer defined above to the network links
+    links.setRenderer(renderer)
 
 def applyGradient(regions, target_field):
     '''
@@ -167,15 +235,26 @@ def main():
     top_dir = getTopDir()
     if top_dir is None:
         exit()
+        
+
 
     # Plot and save total domestic imports
-    regions = readRegions(f'{top_dir}/data/FAF5_regions_with_tonnage/FAF5_regions_with_tonnage.shp', 'FAF5 Regions Imports')
-    applyGradient(regions, 'Total Impo')
-    saveMap([regions], 'Total Domestic Imports', 'Imports [tons/year]', 'total_domestic_imports', f'{top_dir}/layouts/total_domestic_imports.pdf')
+    regions_import = readRegions(f'{top_dir}/data/FAF5_regions_with_tonnage/FAF5_regions_with_tonnage.shp', 'FAF5 Regions Imports')
+    applyGradient(regions_import, 'Total Impo')
+    #saveMap([regions], 'Total Domestic Imports', 'Imports [tons/year]', 'total_domestic_imports', f'{top_dir}/layouts/total_domestic_imports.pdf')
 
     # Plot and save total domestic exports
-    regions = readRegions(f'{top_dir}/data/FAF5_regions_with_tonnage/FAF5_regions_with_tonnage.shp', 'FAF5 Regions Exports')
-    applyGradient(regions, 'Total Expo')
-    saveMap([regions], 'Total Domestic Exports', 'Exports [tons/year]', 'total_domestic_exports', f'{top_dir}/layouts/total_domestic_exports.pdf')
+    regions_export = readRegions(f'{top_dir}/data/FAF5_regions_with_tonnage/FAF5_regions_with_tonnage.shp', 'FAF5 Regions Exports')
+    applyGradient(regions_export, 'Total Expo')
+    #saveMap([regions], 'Total Domestic Exports', 'Exports [tons/year]', 'total_domestic_exports', f'{top_dir}/layouts/total_domestic_exports.pdf')
+    
+    # Add the highway assignments
+    links = load_highway_links()
+    
+    # Style highway links
+    style_highway_links(links)
+    
+    
+    
 
 main()
---
+++ source/processFAFHighwayData.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Sat Apr 8 22:08:00
+
+@author: danikam
+"""
+
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+import geopandas as gpd
+import geopy
+from tqdm import tqdm, trange
+from pathlib import Path
+
+import time
+
+def mergeShapefile(dest, shapefile_path):
+    '''
+    Reads in the shapefile containing the highway links, and merges it with the highway flux assignments
+
+    Parameters
+    ----------
+    dest (pd.DataFrame): A pandas dataframe containing the highway links
+
+    shapefile_path (string): Path to the shapefile to be joined with the dataframe
+
+    Returns
+    -------
+    merged_Dataframe (pd.DataFrame): Joined dataframe
+    '''
+    shapefile = gpd.read_file(shapefile_path, include_fields=['ID', 'LENGTH', 'DATA', 'VERSION', 'ShapeSTLen', 'geometry'])
+    merged_dataframe = shapefile.merge(dest, on='ID', how='left')
+    
+    # Filter for links where the total tons transported (TOT Tons_22 All) isn't NULL
+    merged_dataframe = merged_dataframe[(~(merged_dataframe['TOT Tons_22 All'].isna())) & (merged_dataframe['TOT Tons_22 All'] > 2000)]
+    
+    #print(merged_dataframe)
+    
+    return merged_dataframe
+    
+def saveShapefile(file, name):
+    '''
+    Saves a pandas dataframe as a shapefile
+
+    Parameters
+    ----------
+    file (pd.DataFrame): Dataframe to be saved as a shapefile
+
+    name (string): Filename to the shapefile save to (must end in .shp)
+
+    Returns
+    -------
+    None
+    '''
+    # Make sure the filename ends in .shp
+    if not name.endswith('.shp'):
+        print("ERROR: Filename for shapefile must end in '.shp'. File will not be saved.")
+        exit()
+    # Make sure the full directory path to save to exists, otherwise create it
+    dir = os.path.dirname(name)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    file.to_file(name)
+
+def get_top_dir():
+    '''
+    Gets the path to the top level of the git repo (one level up from the source directory)
+        
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    top_dir (string): Path to the top level of the git repo
+        
+    NOTE: None
+    '''
+    source_path = Path(__file__).resolve()
+    source_dir = source_path.parent
+    top_dir = os.path.dirname(source_dir)
+    return top_dir
+    
+top_dir = get_top_dir()
+    
+def read_highway_assignments():
+    '''
+    Reads in the FAF5 highway assignments as a dataframe, and gets the columns of interest
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    highway_assignments_filtered_df (pd.DataFrame): Dataframe containing the highway link IDs and any other columns of interest
+    '''
+    
+    highway_assignments_df = pd.read_csv(f'{top_dir}/data/FAF5_highway_assignment_results/FAF5_2022_Highway_Assignment_Results/CSV Format/FAF5 Total Truck Flows by Commodity_2022.csv')
+    
+    # Filter for the columns we're interested in
+    highway_assignments_filtered_df = highway_assignments_df.filter(['ID', 'TOT Tons_22 All'], axis=1)
+    
+    return highway_assignments_filtered_df
+    
+def main():
+    # Read in the highway flow assignments for each link
+    df_highway_assignments = read_highway_assignments()
+    #start_time = time.time()
+    
+    # Merge the highway flow assignments in with the shapefile containing the highway links
+    merged_dataframe = mergeShapefile(df_highway_assignments, f'{top_dir}/data/FAF5_network_links/Freight_Analysis_Framework_(FAF5)_Network_Links.shp')
+    
+    #print(f'Merging took {time.time() - start_time} seconds')
+    
+    # Save the merged shapefile
+    saveShapefile(merged_dataframe, f'{top_dir}/data/highway_assignment_links/highway_assignment_links.shp')
+        
+main()
