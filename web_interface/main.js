975417ca87e4667325bd185b7c0b809aa0b2d10c
Danika MacDonell <danikamacdonell@dhcp-10-29-245-0.dyn.mit.edu>
2023-09-15 18:38:04 -0400

    Added functionality to apply a size gradient to point-like features, and a preliminary user interface to toggle layers

--- web_interface/main.js
+++ web_interface/main.js
@@ -1,12 +1,13 @@
 var vectorLayers = [];
 var map;
 var attributeBounds = {}; // Object to store min and max attribute values for each shapefile
-
+const pointSizeFactor = 10;
 
 const shapefileLabels = {
   'electricity_rates_by_state_merged': 'Electricity rate (cents/kWh)',
   'US_elec': 'DCFC Charging Stations',
   'highway_assignment_links_single_unit': 'Highway Freight Flows (annual tons/link)',
+  'electrolyzer_operational': 'Operational Electrolyzers',
 };
 
 // Key: shapefile name, Value: boolean indicating whether to apply a gradient
@@ -14,11 +15,13 @@ const gradientFlags = {
   'electricity_rates_by_state_merged': true,
   'US_elec': false,
   'highway_assignment_links_single_unit': true,
+  'electrolyzer_operational': true,
 };
 
 const gradientAttributes = {
   'electricity_rates_by_state_merged': 'Cents_kWh',
   'highway_assignment_links_single_unit': 'Tot Tons',
+  'electrolyzer_operational': 'Power_kW',
 };
 
 // Key: shapefile name, Value: color to use
@@ -26,8 +29,11 @@ const shapefileColors = {
   'electricity_rates_by_state_merged': 'red',
   'US_elec': 'blue',
   'highway_assignment_links_single_unit': 'black',
+  'electrolyzer_operational': 'purple',
 };
 
+// Create a mapping between layer keys and drop-down values
+const layerMapping = {};
 
 // Fetch shapefile data from the Flask app
 fetch('/get_shapefiles')
@@ -40,16 +46,16 @@ fetch('/get_shapefiles')
   .then(data => {
     console.log('Fetched data:', data); // Debug log to check what's returned
     let allFeatures = [];
-    
-      for (const [key, value] of Object.entries(data)) {
-        const features = new ol.format.GeoJSON().readFeatures(value, {
-          dataProjection: 'EPSG:3857',
-          featureProjection: 'EPSG:3857',
-        });
 
-        const attributeKey = key.split(".")[0];
-        const attributeName = gradientAttributes[attributeKey];
-        
+    for (const [key, value] of Object.entries(data)) {
+      const features = new ol.format.GeoJSON().readFeatures(value, {
+        dataProjection: 'EPSG:3857',
+        featureProjection: 'EPSG:3857',
+      });
+
+      const attributeKey = key.split(".")[0];
+      const attributeName = gradientAttributes[attributeKey];
+
         const minVal = Math.min(...features.map(f => f.get(attributeName) || Infinity));
         const maxVal = Math.max(...features.map(f => f.get(attributeName) || -Infinity));
 
@@ -59,20 +65,80 @@ fetch('/get_shapefiles')
           source: new ol.source.Vector({
             features: features,
           }),
-          style: createStyleFunction(gradientAttributes[key.split(".")[0]], key)
+          style: createStyleFunction(gradientAttributes[key.split(".")[0]], key),
+          key: key.split(".")[0], // Set the key property with the shapefile name without extension
         });
 
-        vectorLayers.push(vectorLayer);
-      }
-    
+      vectorLayers.push(vectorLayer);
+
+      // Create a mapping between the layer key and the drop-down value
+      layerMapping[key.split(".")[0]] = key.split(".")[0];
+    }
+
+    // Populate the layer selection drop-down using the mapping
+    populateLayerDropdown(layerMapping);
+
     initMap();
-      
+
     // Update the legend after layers have been processed
     updateLegend(data);
   })
   .catch(error => {
     console.log('Fetch Error:', error);
   });
+
+// Attach the updateSelectedLayers function to the button click event
+function attachEventListeners() {
+  const applyButton = document.querySelector('button');
+  applyButton.addEventListener('click', updateSelectedLayers);
+}
+
+attachEventListeners();
+
+// Function to populate the layer selection drop-down using the mapping
+function populateLayerDropdown(mapping) {
+  const layerDropdown = document.getElementById("layer-dropdown");
+
+  // Clear existing options
+  layerDropdown.innerHTML = "";
+
+  // Add an "All Layers" option
+  const allOption = document.createElement("option");
+  allOption.value = "all";
+  allOption.textContent = "All Layers";
+  layerDropdown.appendChild(allOption);
+
+  // Add options based on the mapping
+  for (const key in mapping) {
+    const layerOption = document.createElement("option");
+    layerOption.value = key;
+    layerOption.textContent = mapping[key];
+    layerDropdown.appendChild(layerOption);
+  }
+}
+
+// Function to update the selected layers on the map
+function updateSelectedLayers() {
+  const selectedLayers = [];
+  const layerDropdown = document.getElementById("layer-dropdown");
+
+  // Get selected options from the drop-down
+  for (const option of layerDropdown.options) {
+    if (option.selected) {
+      selectedLayers.push(option.value);
+    }
+  }
+
+  // Iterate through the vectorLayers and set visibility
+  vectorLayers.forEach((layer) => {
+    const key = layer.get("key"); // Get the key property
+    if (selectedLayers.includes(key) || selectedLayers.includes("all")) {
+      layer.setVisible(true);
+    } else {
+      layer.setVisible(false);
+    }
+  });
+}
 
 function createStyleFunction(attributeName, shapefileName) {
   return function(feature) {
@@ -88,7 +154,36 @@ function createStyleFunction(attributeName, shapefileName) {
 
     const geometryType = feature.getGeometry().getType();
 
-    if (useGradient && bounds && (geometryType === 'Polygon' || geometryType === 'MultiPolygon')) {
+    if (geometryType === 'Point' || geometryType === 'MultiPoint') {
+      if (useGradient && bounds) {
+        const minSize = 2; // Minimum point size
+        const maxSize = 10; // Maximum point size
+
+        // Calculate the point size based on the attribute value and bounds
+        const pointSize = minSize + ((maxSize - minSize) * (attributeValue - bounds.min) / (bounds.max - bounds.min));
+
+        return new ol.style.Style({
+          image: new ol.style.Circle({
+            radius: pointSize,
+            fill: new ol.style.Fill({
+              color: layerColor,
+            }),
+          }),
+          zIndex: 10, // Higher zIndex so points appear above polygons
+        });
+      } else {
+        // Use a default point style if no gradient or bounds are available
+        return new ol.style.Style({
+          image: new ol.style.Circle({
+            radius: 2, // Default point size
+            fill: new ol.style.Fill({
+              color: layerColor,
+            }),
+          }),
+          zIndex: 10, // Higher zIndex so points appear above polygons
+        });
+      }
+    } else if (useGradient && bounds && (geometryType === 'Polygon' || geometryType === 'MultiPolygon')) {
       const component = Math.floor(255 - (255 * (attributeValue - bounds.min) / (bounds.max - bounds.min)));
       const fillColor = `rgb(255, ${component}, ${component})`;
         
@@ -125,17 +220,7 @@ function createStyleFunction(attributeName, shapefileName) {
           zIndex: 5  // zIndex between points and polygons
         });
       }
-       } else {
-    return new ol.style.Style({
-      image: new ol.style.Circle({
-        radius: 3,
-        fill: new ol.style.Fill({
-          color: layerColor,  // use the color defined for this shapefile
-        }),
-      }),
-      zIndex: 10 // Higher zIndex so points appear above polygons
-    });
-  }
+       }
 
     if (attributeValue === null || attributeValue === undefined) {
       return null;  // Skip features with undefined or null values
@@ -326,16 +411,62 @@ function updateLegend(data) {
             symbolContainer.appendChild(constantCanvas);
             symbolContainer.style.marginRight = "40px";
       }
-    } else {
-      // Point symbol
-      ctx.fillStyle = layerColor; // Use the fetched color
-      ctx.beginPath();
-      ctx.arc(25, 5, 3, 0, Math.PI * 2);
-      ctx.fill();
-      canvas.style.marginLeft = "30px";  // Shift canvas to align the center
-      symbolContainer.appendChild(canvas);
-      symbolContainer.style.marginRight = "40px";
-    }
+    } else if (isPointLayer(vectorLayers[index])) { // this block is for point-like geometries
+  // check if gradient should be used for points
+  if (useGradient && bounds) {
+    // Minimum value and minimum point size
+    const minVal = bounds.min > 100 ? bounds.min.toExponential(2) : bounds.min;
+    const minDiv = document.createElement("div");
+    minDiv.innerText = minVal.toString();
+    minDiv.style.marginRight = "5px";
+    symbolContainer.appendChild(minDiv);
+
+    // Canvas to draw points
+    const minPointSize = 3;  // Minimum size (can set according to your needs)
+    const maxPointSize = 10; // Maximum size (can set according to your needs)
+
+    // Create canvas for the minimum point size
+    const minPointCanvas = document.createElement("canvas");
+    minPointCanvas.width = 20;
+    minPointCanvas.height = 20;
+    const minCtx = minPointCanvas.getContext("2d");
+
+    minCtx.fillStyle = layerColor;
+    minCtx.beginPath();
+    minCtx.arc(10, 10, minPointSize, 0, Math.PI * 2);
+    minCtx.fill();
+    symbolContainer.appendChild(minPointCanvas);
+
+    // Create canvas for the maximum point size
+    const maxPointCanvas = document.createElement("canvas");
+    maxPointCanvas.width = 20;
+    maxPointCanvas.height = 20;
+    const maxCtx = maxPointCanvas.getContext("2d");
+
+    maxCtx.fillStyle = layerColor;
+    maxCtx.beginPath();
+    maxCtx.arc(10, 10, maxPointSize, 0, Math.PI * 2);
+    maxCtx.fill();
+    symbolContainer.appendChild(maxPointCanvas);
+
+    // Maximum value
+    const maxVal = bounds.max > 100 ? bounds.max.toExponential(2) : bounds.max;
+    const maxDiv = document.createElement("div");
+    maxDiv.innerText = maxVal.toString();
+    maxDiv.style.marginLeft = "5px";
+    symbolContainer.appendChild(maxDiv);
+  } else {
+    // code for constant size points
+    ctx.fillStyle = layerColor;
+    ctx.beginPath();
+    ctx.arc(25, 5, 3, 0, Math.PI * 2);
+    ctx.fill();
+    canvas.style.marginLeft = "30px";  // Shift canvas to align the center
+    symbolContainer.appendChild(canvas);
+  }
+
+  symbolContainer.style.marginRight = "40px";
+}
 
     layerDiv.appendChild(symbolContainer);
 
