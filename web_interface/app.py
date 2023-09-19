066cd6144241e9a4890cb250d36e95dc0eeafc2c
Danika MacDonell <danikamacdonell@dhcp-10-29-205-195.dyn.MIT.EDU>
2023-09-19 15:32:19 -0400

    Updated to allow for reading shapefiles from the data dir rather than the top level of the web_interface dir

--- web_interface/app.py
+++ web_interface/app.py
@@ -15,14 +15,20 @@ def index():
 def serve_js():
     return send_from_directory('.', 'main.js')
 
-shapefiles = ["electricity_rates_by_state_merged.shp", "US_elec.shp", "highway_assignment_links_single_unit.shp", "electrolyzer_operational.shp"]
+shapefile_directory = "../data"
+shapefiles = [
+    os.path.join(shapefile_directory, "electricity_rates_merged/electricity_rates_by_state_merged.shp"),
+    os.path.join(shapefile_directory, "Fuel_Corridors/US_elec/US_elec.shp"),
+    os.path.join(shapefile_directory, "highway_assignment_links/highway_assignment_links_single_unit.shp"),
+    os.path.join(shapefile_directory, "hydrogen_hubs/shapefiles/electrolyzer_operational.shp")]
 
 @app.route('/get_shapefiles')
 def get_shapefiles():
     shapefile_data = {}
-    for sf in shapefiles:
-        shapefile = gpd.read_file(sf).to_crs(epsg=3857)
-        shapefile_data[sf] = json.loads(shapefile.to_json())
+    for sf_path in shapefiles:
+        sf_filename = os.path.basename(sf_path)
+        shapefile = gpd.read_file(sf_path).to_crs(epsg=3857)
+        shapefile_data[sf_filename] = json.loads(shapefile.to_json())
     return jsonify(shapefile_data)
 
 if __name__ == '__main__':
