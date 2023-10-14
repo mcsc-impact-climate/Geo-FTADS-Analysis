8c885dad5faaa358210a3cad5a0d37f408fb613f
damacdon <danikam1@uvic.ca>
2023-10-14 19:42:19 -0400

    Fixed indentation in SimplifyGeojsons.py

--- source/SimplifyGeojsons.py
+++ source/SimplifyGeojsons.py
@@ -11,56 +11,56 @@ import os
 # Create an ordered dictionary to maintain the order of items
 shapefiles = OrderedDict()
 
-# # Total domestic Imports and Exports
-# shapefiles['Truck Imports and Exports'] = os.path.join("../data/Point2Point_outputs/mode_truck_commodity_all_origin_all_dest_all.shp")
-#
-# # Grid emission intensity
-# shapefiles['Grid Emission Intensity'] = os.path.join("../data/egrid2020_subregions_merged/egrid2020_subregions_merged.shp")
-#
-# # Commercial electricity price by state
-# shapefiles['Commercial Electricity Price'] = os.path.join("../data/electricity_rates_merged/electricity_rates_by_state_merged.shp")
-#
-# #Maximum demand charges from NREL
-# shapefiles['Maximum Demand Charge'] = os.path.join("../data/electricity_rates_merged/demand_charges_merged.shp")
-#
-# # Highway flows
-# shapefiles['Highway Flows (Interstate)'] = os.path.join("../data/highway_assignment_links/highway_assignment_links_interstate.shp")
-# shapefiles['Highway Flows (SU)'] = os.path.join("../data/highway_assignment_links/highway_assignment_links_single_unit.shp")
-# shapefiles['Highway Flows (CU)'] = os.path.join("../data/highway_assignment_links/highway_assignment_links_combined_unit.shp")
+# Total domestic Imports and Exports
+shapefiles['Truck Imports and Exports'] = os.path.join("data/Point2Point_outputs/mode_truck_commodity_all_origin_all_dest_all.shp")
 
-# # Alternative fueling stations along highway corridors
-# shapefiles['DCFC Chargers'] = os.path.join("../data/Fuel_Corridors/US_elec/US_elec.shp")
-# shapefiles['Hydrogen Stations'] = os.path.join("../data/Fuel_Corridors/US_hy/US_hy.shp")
-# shapefiles['LNG Stations'] = os.path.join("../data/Fuel_Corridors/US_lng/US_lng.shp")
-# shapefiles['CNG Stations'] = os.path.join("../data/Fuel_Corridors/US_cng/US_cng.shp")
-# shapefiles['LPG Stations'] = os.path.join("../data/Fuel_Corridors/US_lpg/US_lpg.shp")
-#
-# # Hydrogen hubs
-# shapefiles['Operational Electrolyzers'] = os.path.join("../data/hydrogen_hubs/shapefiles/electrolyzer_operational.shp")
-# shapefiles['Installed Electrolyzers'] = os.path.join("../data/hydrogen_hubs/shapefiles/electrolyzer_installed.shp")
-# shapefiles['Planned Electrolyzers'] = os.path.join("../data/hydrogen_hubs/shapefiles/electrolyzer_planned_under_construction.shp")
-# shapefiles['Hydrogen from Refineries'] = os.path.join("../data/hydrogen_hubs/shapefiles/refinery.shp")
-#
-# # DOE-funded heavy duty vehicle infrastructure projects
-# shapefiles['East Coast ZEV Corridor'] = os.path.join("../data/hd_zev_corridors/eastcoast.shp")
-# shapefiles['Midwest ZEV Corridor'] = os.path.join("../data/hd_zev_corridors/midwest.shp")
-# shapefiles['Houston to LA H2 Corridor'] = os.path.join("../data/hd_zev_corridors/h2la.shp")
-# shapefiles['I-710 EV Corridor'] = os.path.join("../data/hd_zev_corridors/la_i710.shp")
-# shapefiles['Northeast EV Corridor'] = os.path.join("../data/hd_zev_corridors/northeast.shp")
-# shapefiles['Bay Area EV Roadmap'] = os.path.join("../data/hd_zev_corridors/bayarea.shp")
-# shapefiles['Salt Lake City Region EV Plan'] = os.path.join("../data/hd_zev_corridors/saltlake.shp")
-#
-# # Truck stop parking locations
-# shapefiles['Truck Stop Locations'] = os.path.join("../data/Truck_Stop_Parking/Truck_Stop_Parking.shp")
-#
-# # Principal ports
-# shapefiles['Principal Ports'] = os.path.join("../data/Principal_Ports/Principal_Port.shp")
-#
-# # Regional Incentives
-# shapefiles['State-Level Incentives and Regulations'] = os.path.join("../data/incentives_and_regulations_merged/all_incentives_and_regulations.shp")
+# Grid emission intensity
+shapefiles['Grid Emission Intensity'] = os.path.join("data/egrid2020_subregions_merged/egrid2020_subregions_merged.shp")
+
+# Commercial electricity price by state
+shapefiles['Commercial Electricity Price'] = os.path.join("data/electricity_rates_merged/electricity_rates_by_state_merged.shp")
+
+#Maximum demand charges from NREL
+shapefiles['Maximum Demand Charge'] = os.path.join("data/electricity_rates_merged/demand_charges_merged.shp")
+
+# Highway flows
+shapefiles['Highway Flows (Interstate)'] = os.path.join("data/highway_assignment_links/highway_assignment_links_interstate.shp")
+shapefiles['Highway Flows (SU)'] = os.path.join("data/highway_assignment_links/highway_assignment_links_single_unit.shp")
+shapefiles['Highway Flows (CU)'] = os.path.join("data/highway_assignment_links/highway_assignment_links_combined_unit.shp")
+
+# Alternative fueling stations along highway corridors
+shapefiles['DCFC Chargers'] = os.path.join("data/Fuel_Corridors/US_elec/US_elec.shp")
+shapefiles['Hydrogen Stations'] = os.path.join("data/Fuel_Corridors/US_hy/US_hy.shp")
+shapefiles['LNG Stations'] = os.path.join("data/Fuel_Corridors/US_lng/US_lng.shp")
+shapefiles['CNG Stations'] = os.path.join("data/Fuel_Corridors/US_cng/US_cng.shp")
+shapefiles['LPG Stations'] = os.path.join("data/Fuel_Corridors/US_lpg/US_lpg.shp")
+
+# Hydrogen hubs
+shapefiles['Operational Electrolyzers'] = os.path.join("data/hydrogen_hubs/shapefiles/electrolyzer_operational.shp")
+shapefiles['Installed Electrolyzers'] = os.path.join("data/hydrogen_hubs/shapefiles/electrolyzer_installed.shp")
+shapefiles['Planned Electrolyzers'] = os.path.join("data/hydrogen_hubs/shapefiles/electrolyzer_planned_under_construction.shp")
+shapefiles['Hydrogen from Refineries'] = os.path.join("data/hydrogen_hubs/shapefiles/refinery.shp")
+
+# DOE-funded heavy duty vehicle infrastructure projects
+shapefiles['East Coast ZEV Corridor'] = os.path.join("data/hd_zev_corridors/eastcoast.shp")
+shapefiles['Midwest ZEV Corridor'] = os.path.join("data/hd_zev_corridors/midwest.shp")
+shapefiles['Houston to LA H2 Corridor'] = os.path.join("data/hd_zev_corridors/h2la.shp")
+shapefiles['I-710 EV Corridor'] = os.path.join("data/hd_zev_corridors/la_i710.shp")
+shapefiles['Northeast EV Corridor'] = os.path.join("data/hd_zev_corridors/northeast.shp")
+shapefiles['Bay Area EV Roadmap'] = os.path.join("data/hd_zev_corridors/bayarea.shp")
+shapefiles['Salt Lake City Region EV Plan'] = os.path.join("data/hd_zev_corridors/saltlake.shp")
+
+# Truck stop parking locations
+shapefiles['Truck Stop Locations'] = os.path.join("data/Truck_Stop_Parking/Truck_Stop_Parking.shp")
+
+# Principal ports
+shapefiles['Principal Ports'] = os.path.join("data/Principal_Ports/Principal_Port.shp")
+
+# Regional Incentives
+shapefiles['State-Level Incentives and Regulations'] = os.path.join("data/incentives_and_regulations_merged/all_incentives_and_regulations.shp")
 
 # Truck charger infrastructure savings
-shapefiles['Truck charging'] = os.path.join("../data/Truck_Stop_Parking/Truck_Stop_Parking_Along_Interstate_with_min_chargers.shp")
+shapefiles['Truck charging'] = os.path.join("data/Truck_Stop_Parking/Truck_Stop_Parking_Along_Interstate_with_min_chargers.shp")
 
 geojson_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '../web/geojsons_simplified'))
 
